f8e85dca1e3fc8d60f35bcf051781b2aeaa79c6b
Zachery Hostens <zacheryph@gmail.com>
2016-04-14 20:22:42 -0500

    use the pam handler our Handle has

--- conversation.go
+++ conversation.go
@@ -65,7 +65,7 @@ func (hdl Handle) Conversation(_msgs ...Message) ([]string, error) {
 		resp = append(resp, &C.struct_pam_response{})
 	}
 
-	code := C.do_conv(hdl.ptr(), C.int(len(_msgs)), &msg[0], &resp[0])
+	code := C.do_conv(hdl.pamh, C.int(len(_msgs)), &msg[0], &resp[0])
 	if code != C.PAM_SUCCESS {
 		return nil, fmt.Errorf("Got non-success from the function: %d", code)
 	}
